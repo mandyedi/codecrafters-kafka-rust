2d023a1747dab4aaf2bdd9a13f62b5bdc11c0ada
Eduard Mandy <mandyedi@gmail.com>
2024-09-29 22:40:27 +0200

    Stage: Parse Correlation ID

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 #![allow(unused_imports)]
-use std::{io::{Read, Write}, net::TcpListener};
+use std::{io::{Read, Write}, net::TcpListener, mem};
+use bytes::{Buf, BufMut};
+
+struct RequestHeader {
+    request_api_key: i16,
+    request_api_version: i16,
+    correlation_id: i32,
+}
+
+struct ResponseHeader {
+    message_length: i32,
+    correlation_id: i32,
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,10 +21,33 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
-                let response: [u8; 8] = [0, 0, 0, 0, 0,  0,  0,  7];
-                _stream.write(&response).unwrap();
+
+                let mut length: [u8; 4] = [0, 0, 0, 0];
+                stream.read_exact(&mut length).unwrap();
+                let length = i32::from_be_bytes(length) as usize;
+
+                let mut request = vec![0; length];
+                stream.read_exact(&mut request).unwrap();
+                
+                let mut request = request.as_slice();
+                let request_header = RequestHeader {
+                    request_api_key: request.get_i16(),
+                    request_api_version: request.get_i16(),
+                    correlation_id: request.get_i32()
+                };
+
+                let response_header = ResponseHeader {
+                    message_length: 0,
+                    correlation_id: request_header.correlation_id,
+                };
+
+                let mut response = Vec::with_capacity(8);
+                response.put_i32(response_header.message_length);
+                response.put_i32(response_header.correlation_id);
+
+                stream.write_all(&response).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
